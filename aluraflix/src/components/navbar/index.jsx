b750dc2f719eb06462d69acae5a6cc0b05bea55c
Adolfo <sanchezlopezadolfo@gmail.com>
2025-01-06 17:23:20 -0500

    Componente NavBar

---
+++ aluraflix/src/components/navbar/index.jsx
@@ -0,0 +1,18 @@
+import styles from "./navbar.module.css";
+import logo from "./assets/logo.svg";
+
+function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarLogo}>
+        <img src={logo} alt="Logo AluraFlix" />
+      </div>
+      <div className={styles.navbarButtons}>
+        <button className={styles.navbarButtonHome}>Home</button>
+        <button className={styles.navbarButtonNewVideo}>Nuevo video</button>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
