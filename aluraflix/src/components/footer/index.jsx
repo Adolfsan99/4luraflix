cb0e9a3cd95bcd35260838692f5fc53bfd9650b9
Adolfo <sanchezlopezadolfo@gmail.com>
2025-01-06 18:51:28 -0500

    Componente Footer

--- aluraflix/src/components/footer/index.jsx
+++ aluraflix/src/components/footer/index.jsx
@@ -1,7 +1,12 @@
 import styles from "./footer.module.css";
+import logoFooter from "./assets/logoFooter.svg";
 
 function Footer() {
-  return <h1 className={styles.Footer}>Pie de pagina</h1>;
+  return (
+    <h1 className={styles.footer}>
+      <img src={logoFooter} alt="Logo footer AluraFlix" />
+    </h1>
+  );
 }
 
 export default Footer;
